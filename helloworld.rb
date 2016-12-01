95fd386b8f2c684f2cf8da37cddbfba54cc6c29a
Simon <scheng365@gmail.com>
2016-11-30 23:17:30 -0500

    More loops

--- helloworld.rb
+++ helloworld.rb
@@ -101,10 +101,9 @@ greeting = gets.chomp #Chomp removes the newline
 case greeting
 when "French","french"
 	puts "Bonjour"
-	exit
 when "English","english"
 	puts "Hello"
-	exit
+	
 else "Not Recognized"
 	puts "Not Recognized"
 end
@@ -115,7 +114,7 @@ puts (age >= 50) ? "Old" : "Young"
 
 #Loop do
 x = 1
-
+puts "Do Loop"
 loop do
 	x+=1
 
@@ -124,11 +123,43 @@ loop do
 
 	break if x>= 10
 end
+puts ""
 
 #While Loop
+puts "While Loop"
+y = 1
 
+while y <= 10
+	y += 1
+	next unless (y % 2) == 0
+	puts y
+end
+puts ""
 
+#Until loop
+puts "Until loop"
+a = 1
+until a >= 10
+	a+= 1
+	next unless (a%2) == 0
+	puts a
+end
+puts ""
 
+#For loop
+puts "For loop"
+numbers  = [1,2,3,4,5]
+for number in numbers
+	print "#{number}, "
+end
+puts ""
+
+#For loop 
+puts "For Loop #2"
+groceries = ["Banana, Apple, Pear"]
+groceries.each do |food|
+	puts "Get some #{food}"
+end
 
 puts ""
 
